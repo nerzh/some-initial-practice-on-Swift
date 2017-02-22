5130f4d0a7b0b646c7cda6003565120973aa8126
woodcrust <roboucrop@gmail.com>
2017-02-22 13:47:59 +0200

    add new extensions

--- my_extensions.swift
+++ my_extensions.swift
@@ -1,4 +1,14 @@
+import UIKit
+import Foundation
+
 extension String {
+    func heightSelf(_ width: CGFloat, _ font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: CGFloat.greatestFiniteMagnitude)
+        let rect = (self as NSString).boundingRect(with: constraintRect, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        
+        return rect.height
+    }
+    
     subscript(index: Int) -> String {
         get {
             return String(self[self.index(self.startIndex, offsetBy: index)])
@@ -29,8 +39,82 @@ extension String {
         get { return String(self[self.startIndex])}
     }
     
-    //    func findIndex(of: Character) -> Int {
-    //        let range: Range<String.Index> = self.range(of: "a")!
-    //        return self.distance(from: self.startIndex, to: range.lowerBound)
-    //    }
+    func findIndex(of: Character) -> Int {
+        let range: Range<String.Index> = self.range(of: String(of))!
+        return self.distance(from: self.startIndex, to: range.lowerBound)
+    }
+    
+    var count : Int {
+        get { return self.distance(from: self.startIndex, to: self.endIndex) }
+    }
+    
+    var size : Int {
+        get { return self.count }
+    }
+    
+    // regexp temporary betta version
+    subscript(pattern: String) -> String? {
+        get {
+            var result : String?
+            
+            do {
+                let regexp = try NSRegularExpression(pattern: pattern)
+                let matches = regexp.matches(in: self, options: [], range: NSRange(location: 0, length: self.count))
+                
+                for match in matches {
+                    for count in 0..<match.numberOfRanges {
+                        let rangeOfMatch = match.rangeAt(count)
+                        
+                        let resultRange = self.index(self.startIndex, offsetBy: rangeOfMatch.location) ..<
+                            self.index(self.startIndex, offsetBy: rangeOfMatch.location+rangeOfMatch.length)
+                        
+                        return self.substring(with: resultRange)
+                    }
+                }
+            } catch {
+                result = nil
+            }
+            
+            return result
+        }
+    }
+    
+    // regexp2 temporary betta version
+    func regexp(_ pattern: String) -> [Int:String]? {
+        var result : [Int:String]? = [Int:String]()
+        
+        do {
+            let regexp = try NSRegularExpression(pattern: pattern)
+            let matches = regexp.matches(in: self, options: [], range: NSRange(location: 0, length: self.count))
+            for match in matches {
+                for count in 0..<match.numberOfRanges {
+                    let rangeOfMatch = match.rangeAt(count)
+                    
+                    let resultRange = self.index(self.startIndex, offsetBy: rangeOfMatch.location) ..<
+                        self.index(self.startIndex, offsetBy: rangeOfMatch.location+rangeOfMatch.length)
+                    
+                    result?[count] = self.substring(with: resultRange)
+                }
+                return result
+            }
+            
+            return nil
+        } catch { result = nil }
+        
+        return result
+    }
 }
+
+// for keyboard hidden
+extension UIViewController {
+    func setKeyboardEvent() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
